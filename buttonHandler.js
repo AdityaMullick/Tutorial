5cc5f2f793d44a5d7e7199e89783553d524a57dc
Aditya Mullick <adityamullick98@yahoo.com>
2018-06-26 20:20:46 -0700

    first commit

--- buttonHandler.js
+++ buttonHandler.js
@@ -1,23 +1,133 @@
 $(document).ready(function() {
-  $("#simple").click(function() {
-    $("#tutorialPane").attr('src',"https://forward.ucsd.edu/tutorial/01-simple-html.html");
-  });
-  $("#visual").click(function() {
-    $("#tutorialPane").attr('src',"visual.html");
-  });
-  $("#dynamic").click(function() {
-    $("#tutorialPane").attr('src',"dynamic.html");
-  });
-  $("#input").click(function() {
-    $("#tutorialPane").attr('src',"user-input.html");
-  });
-  $("#parameters").click(function() {
-    $("#tutorialPane").attr('src',"");
-  });
-  $("#singleBrowse").click(function() {
-    $("#tutorialPane").attr('src',"");
-  });
-  $("#multipleBrowse").click(function() {
-    $("#tutorialPane").attr('src',"");
-  });
+    var current = 0;
+    var options = ['simple', 'visual', 'dynamic', 'input', 'parameters','singleBrowse', 'multipleBrowse'];
+    $("#simple").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="simple.htm" ></object>';
+        //set the correct examples
+//        var html_editor = ace.edit("html");
+        window.location.href = "index.html";
+
+
+
+        current = 0;
+
+    });
+    $("#visual").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem"  style="min-width:100%; min-height: 101%;" type="text/html" data="visual.html" ></object>';
+        current = 1;
+        window.location.href = "visual_units.html";
+
+    });
+    $("#dynamic").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="dynamic.html" ></object>';
+        window.location.href = "dynamic_data.html";
+        current = 2;
+
+
+    });
+    $("#input").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="user-input.html" ></object>';
+        current = 3;
+        window.location.href = "user_input.html";
+
+
+    });
+    $("#parameters").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="dynamic.html" ></object>';
+        current = 4;
+        window.location.href = "url_parameters.html";
+
+    });
+    $("#singleBrowse").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="dynamic.html" ></object>';
+        current = 5;
+        window.location.href = "single_input.html";
+
+
+    });
+    $("#multipleBrowse").click(function() {
+        document.getElementById("container").innerHTML='<object class = "elem" style="min-width:100%; min-height: 101%;" type="text/html" data="dynamic.html" ></object>';
+        current = 6;
+        window.location.href = "multiple_input.html";
+
+
+    });
+    $("#nextButton").click(function() {
+        console.log(window.location.href);
+
+        var url = window.location.href;
+        if(url.indexOf("index.html") != -1) {
+            document.getElementById("visual").click();
+
+        }
+        else if(url.indexOf("visual_units.html") != -1) {
+            document.getElementById("dynamic").click();
+
+
+        }
+        else if(url.indexOf("dynamic_data.html") != -1) {
+            document.getElementById("input").click();
+
+
+        }
+        else if(url.indexOf("user_input.html") != -1) {
+            document.getElementById("parameters").click();
+
+
+        }
+        else if(url.indexOf("url_parameters.html") != -1) {
+            document.getElementById("singleBrowse").click();
+
+        }
+        else if(url.indexOf("single_input.html") != -1) {
+            document.getElementById("multipleBrowse").click();
+
+
+        }
+
+
+        /*if(current < options.length - 1) {
+            var file = options[current + 1];
+            console.log(current);
+            console.log(file);
+            document.getElementById(file).click();
+            current++;
+        }*/
+
+    });
+    $("#previous").click(function() {
+        console.log(window.location.href);
+
+        var url = window.location.href;
+        if(url.indexOf("visual_units.html") != -1) {
+            document.getElementById("simple").click();
+
+        }
+        else if(url.indexOf("dynamic_data.html") != -1) {
+            document.getElementById("visual").click();
+
+
+        }
+        else if(url.indexOf("user_input.html") != -1) {
+            document.getElementById("dynamic").click();
+
+
+        }
+        else if(url.indexOf("url_parameters.html") != -1) {
+            document.getElementById("input").click();
+
+
+        }
+        else if(url.indexOf("single_input.html") != -1) {
+            document.getElementById("parameters").click();
+
+        }
+        else if(url.indexOf("multiple_input.html") != -1) {
+            document.getElementById("singleBrowse").click();
+
+
+        }
+
+
+        });
 });
--- js/ide/main-ide.js
+++ js/ide/main-ide.js
@@ -6,17 +6,42 @@ requirejs(['require-config'], function() {
         // Forward.display('js/demos/first-demo-before/template.fwd', 'myID');
 
             var template_editor = ace.edit("template");
+            var action_editor = ace.edit("action");
+            var html_editor = ace.edit("html");
+
             // template_editor.session.setMode("ace/mode/html");
             // template_editor.getSession().setUseWorker(false);
             var content = template_editor.getValue();
+            var action_content = action_editor.getValue();
+            console.log(action_content);
+            var html_content = html_editor.getValue();
+            var myIDElem = document.getElementById("runPane");
+            myIDElem.innerHTML = html_content;
+
+            var template = action_editor.getValue();
+            eval(template);
+
+
+
             console.log(content);
-            Forward._display_template(content, {}, 'myID');
+            //Forward._display_template(content, {}, 'myID');
             $('#run-button').click( function(e) {
+                // parse html tab content and set to the bottom pane
+                var html_content = html_editor.getValue();
+                var myIDElem = document.getElementById("runPane");
+                myIDElem.innerHTML = html_content;
+
+                // get javascript action and evaluate it
+                var template = action_editor.getValue();
+
                 e.preventDefault();
                 var content = template_editor.getValue();
                 /*your_code_here;*/
-                Forward._display_template(content, {}, 'myID');
+                document.getElementById("myID").innerHTML = "";
+                eval(template);
                 return false; } );
 
     });
+
+
 });
